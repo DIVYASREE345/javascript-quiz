818c7bc44fbf14abc07fd6dec95debd7a3f5d344
Ketunze Brain <brainketunze@gmail.com>
2020-12-23 16:21:20 +0100

    data.js update

--- src/data.js
+++ src/data.js
@@ -65,19 +65,7 @@ const quizData = {
            b: 'alertBox ("Hello World")',
            c: 'alert ("Hello World")',
         },
-        },
-      ],
-    },
-
-    {
-        text: 'How do you write "Hello World" in an alert box?',
-        answers: {
-           a: 'msgBox ("Hello World")',
-           b: 'alertBox ("Hello World")',
-           c: 'alert ("Hello World")',
-        },
-
-       correctAnswer:'c',
+        correctAnswer: 'a',
        resource: [
         {
           text: 'www.w3schools.com',
@@ -141,7 +129,12 @@ const quizData = {
       b: 'Data types',
       c: 'Declaration statements',
         },
-        },
+     correctAnswer: 'c',
+      resource: [
+    {
+      text: 'javascript.info',
+      href: 'https://www.sanfoundry.com/javascript-mcqs-forms-debugging/#google_vignette',
+    },
       ],
   },
 
@@ -212,6 +205,3 @@ const quizData = {
     },
   ],
 };
-};
-
-
