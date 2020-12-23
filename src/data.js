54ea1b8cbde18a2bddd72fec91f56fa4ec0de912
Ketunze Brain <brainketunze@gmail.com>
2020-12-23 14:56:29 +0100

    questions update file

--- src/data.js
+++ src/data.js
@@ -1,22 +1,21 @@
 'use strict';
 
 /* Program Data
-
   in this file you can declare variables to store important data for your program
   the data can only be primitives, objects or arrays
   do not store dom elements in these variables!!!!
     your handlers will query the DOM each time they need to make a change
-
   these variables will be imported by your handlers when necessary
     not by your logic
     not by your listeners
-
 */
+
 
 // this is example data for the starter demo
 //  delete this data when you begin and use the own quiz data instead
+
 /**
- * data that is saved and used between user interactionss
+ * data that is saved and used between user interactions
  * @property {string} separator - the full user artwork
  * @property {string[]} lines - all the lines that have been entered
  */
@@ -44,46 +43,131 @@ const quizData = {
   // the questions in the quiz
   questions: [
     {
-      text: 'What are the different ways to declare a JS variable?',
+      text: "How do you declare a JavaScript variable?",
       answers: {
-        a: 'constant, let, variable',
-        b: 'var, const, let, function',
-        c: 'var, let, const',
+        a: 'var carName',
+        b: 'v carName',
+        c: 'variable carName',
       },
-      correct: 'c',
-      selected: null,
-      links: [
+       correctAnswer: 'a',
+       resource: [
         {
-          text: 'javascript.info',
-          href: 'https://javascript.info/variables',
-        },
-        {
-          text: 'Tyler McGinnis',
-          href: 'https://ui.dev/var-let-const/',
+          text: 'www.w3schools.com',
+          href: 'https://www.w3schools.com/quiztest/result.asp',
         },
       ],
     },
+
     {
-      text: 'What does `typeof` do?',
-      answers: {
-        a: 'changes the type of a primitive value',
-        b: 'returns a string describing the type of a value',
-        c: 'determines if a value is primitive',
-        d: 'can tell the difference between arrays and objects',
-      },
-      correct: 'b',
-      selected: null,
-      links: [
-        {
-          text: 'javascript.info',
-          href: 'https://javascript.info/types#type-typeof',
+        text: 'How do you write "Hello World" in an alert box?',
+        answers: {
+           a: 'msgBox ("Hello World")',
+           b: 'alertBox ("Hello World")',
+           c: 'alert ("Hello World")',
         },
+       correctAnswer:'c',
+       resource: [
         {
-          text: 'MDN',
-          href:
-            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+          text: 'www.w3schools.com',
+          href: 'https://www.w3schools.com/quiztest/result.asp',
         },
       ],
+    },
+    
+    {
+      text: 'Which of the following is not a reserved word in JavaScript?',  
+      answers: {
+            a: 'interface',
+            b: 'program',
+            c: 'throws',
+          },
+       correctAnswer: 'b',
+       resource: [
+        {
+          text: 'javascript.info',
+          href: 'https://mathiasbynens.be/notes/javascript-identifiers',
+        },
+      ],
+    },
+
+  {
+      text: 'What is it called when we make a mistake in the script?',
+    answer: {
+       a: 'Error',
+       b: 'Bug',
+       c: 'Mistake',
+      },
+       correctAnswer: 'b',
+       resource: [
+        {
+          text: 'javascript.info',
+          href: 'https://www.sanfoundry.com/javascript-mcqs-forms-debugging/#google_vignette',
+        },
+      ],
+  },
+
+{
+  text: 'Which of the following is the definition for debugging?',
+  answer: {
+     a: 'Finding bugs',
+     b: 'Fixing bugs',
+     c: 'Both Finding & Fixing bugs',
+      },
+   correctAnswer: 'c',
+   resource: [
+    {
+      text: 'javascript.info',
+      href: 'https://www.sanfoundry.com/javascript-mcqs-forms-debugging/#google_vignette',
+    },
+      ],
+ 
+     },
+     {
+      text: 'The "function" and " var" are known as?',
+      answer: {
+      a: 'Keywords',
+      b: 'Data types',
+      c: 'Declaration statements',
+        },
+       correctAnswer: 'c',
+       resource: [
+        {
+          text: 'javascript.info',
+          href: 'https://www.javatpoint.com/javascript-mcq',
+        },
+      ],
+     
+     },
+     {
+          text: 'Where is the error icon option available?',
+          answer: {
+            a: 'Tools',
+            b: 'Help',
+            c: 'File',
+              },
+           correctAnswer: 'a',
+           resource: [
+        {
+          text: 'javascript.info',
+          href: 'https://www.sanfoundry.com/javascript-mcqs-forms-debugging/#google_vignette',
+        },
+       ],
+         
+      },
+      {
+        text: 'In JavaScript the x===y statement implies that?',
+        answer: {
+          a: 'Both x and y are equal in value, type and reference address as well',
+          b: 'Both are x and y are equal in value only',
+          c: 'Both are equal in the value and data type',
+            },
+         correctAnswer: 'c',
+         resource: [
+      {
+        text: 'javascript.info',
+        href: 'https://www.javatpoint.com/javascript-mcq',
+      },
+     ],
     },
   ],
 };
